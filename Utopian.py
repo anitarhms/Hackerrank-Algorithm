0970fe4ccb4170e496bb94872b0e08497f3962bd
Anita Rahma Sukaningrum <103645850+anitarhms@users.noreply.github.com>
2023-05-09 20:18:27 +0700

    Create Utopian.py

---
+++ Utopian.py
@@ -0,0 +1,39 @@
+#Utopian tree
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'utopianTree' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def utopianTree(n):
+    # Write your code here
+    tinggi=0
+    for i in range(n+1):
+        if i%2==0:
+            tinggi+=1
+        else:
+            tinggi=tinggi*2
+    return tinggi
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = utopianTree(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
