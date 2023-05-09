0801f5b8438bed570d875506b2491c1238e7aa71
Anita Rahma Sukaningrum <103645850+anitarhms@users.noreply.github.com>
2023-05-09 20:17:36 +0700

    Create PDFview.py

---
+++ PDFview.py
@@ -0,0 +1,37 @@
+#designer PDF viewer
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+def designerPdfViewer(h, word):
+    # Write your code here
+    tinggi=0
+    for huruf in range (len(word)):
+        if tinggi<h[ord(word[huruf])-97]:     # ascii a di decimal =97 
+            tinggi=h[ord(word[huruf])-97]
+    return tinggi*len(word)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
